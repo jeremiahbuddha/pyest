abcda1e9a906ac53ae2476fddf3353f44722ab3d
jeremiahbuddha <jeremiahbuddha@gmail.com>
2012-04-07 19:12:34 -0700

    Modified make_data.py to write out dict and list.

--- scripts/make_data.py
+++ scripts/make_data.py
@@ -6,11 +6,19 @@ with open('tracking_data.txt') as f:
     tdata = f.readlines()
 
 # Reformat data as nested python lists
-out = 'TRACKING_DATA = ['
+out = 'TRACKING_LIST = ['
 for line in tdata:
-    out+='\n    [{0},{1},{2},{3}],'.format(line.split()[0],line.split()[1],
+    out+='\n    [{0}, {1}, {2}, {3}],'.format(line.split()[0],line.split()[1],
                                            line.split()[2],line.split()[3] )
-out += '\n    ]'
+out += '\n    ]\n\n'
+
+# Reformat data as dictionary with time as key
+out += 'TRACKING_DICT = {'
+for line in tdata:
+    out+='\n    {0} : [{1}, {2}, {3}],'.format(line.split()[0],line.split()[1],
+                                           line.split()[2],line.split()[3] )
+out += '\n    }'
+
 
 # Write tracking data to importable module
 with open('tracking_data.py','w') as f:
